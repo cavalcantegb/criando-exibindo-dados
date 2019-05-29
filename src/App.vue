0d6fe12544ad2ca5471e91a7d98480065dd9ad23
Guilherme Cavalcante <cavalcante.guilherme91@gmail.com>
2019-05-29 10:40:38 -0300

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+    <h1 v-once v-text="titulo"></h1>
+    <h2>{{subtitulo}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'lv-tarefas',
+  data() {
+    return {
+      titulo: 'Listagem de tarefas',
+      subtitulo: 'Defina uma descricao',
+      tarefas: []
+    }
+  },
+  mounted() {
+    setTimeout( () => this.titulo = "Novo titulo", 2000 )
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
